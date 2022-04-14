90c4c7c47e9f7266c640df41e0d7cf81bf2b1394
Cengiz Bugra Aksakal <aksakalcengiz@hotmail.com>
2022-04-14 14:29:55 +0300

    Functions (continued) lab completed.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(action = "roller-skate") {
+    return `This Saturday, I want to ${action}!`;
+}
+
+function wrapAdjective(arg = "*") {
+    return function(adj = "special") {
+        return `You are ${arg}${adj}${arg}!`;
+    }
+}
+
+// console.log(wrapAdjective()("a hard worker"));
+
+const mondayWork = function(action = "go to the office") {
+    return `This Monday, I will ${action}.`
+};
